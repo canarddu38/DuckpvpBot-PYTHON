85eb6e465e8f85290059d4d1a28c5e7491e01701
canarddu38 <71982379+canarddu38@users.noreply.github.com>
2022-02-28 19:42:59 +0100

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -1,7 +1,7 @@
 import os
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix="!")
+bot = commands.Bot(command_prefix="db!")
 TOKEN = os.getenv("DISCORD_TOKEN")
 
 @bot.event
@@ -11,6 +11,9 @@ async def on_ready():
 @bot.command()
 async def ping(ctx):
     await ctx.send("pong")
+    
+ def ping2(ctx):
+    await ctx.send("pong2")
 
 if __name__ == "__main__":
     bot.run(TOKEN)
