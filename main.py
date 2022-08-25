11f17338bf7dc6485eb28911ccc374539be6b0d2
canarddu38 <71982379+canarddu38@users.noreply.github.com>
2022-08-25 15:18:19 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ intents = discord.Intents.default()
 intents.message_content = True
 
 client = discord.Client(intents=intents)
+TOKEN = os.getenv("DISCORD_TOKEN")
 
 @client.event
 async def on_ready():
@@ -17,4 +18,4 @@ async def on_message(message):
 
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
-client.run(os.getenv('DISCORD_TOKEN'))
+client.run(TOKEN)
