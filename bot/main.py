f1a529d11fc0d3e98565b3c8fda76e3a83becefe
canarddu38 <71982379+canarddu38@users.noreply.github.com>
2022-08-25 15:31:30 +0200

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -1,19 +1,21 @@
 import os
 from discord.ext import commands
+import discord
 
-bot = commands.Bot(command_prefix="db!")
 TOKEN = os.getenv("DISCORD_TOKEN")
 
-@bot.event
-async def on_ready():
-    print(f"Logged in as {bot.user.name}({bot.user.id})")
+class MyClient(discord.Client):
+    @bot.event
+    async def on_ready():
+       print(f"Logged in as {bot.user.name}({bot.user.id})")
 
-@bot.command()
-async def ping(ctx):
-    ctx.send("pong")
+    @bot.command()
+    async def ping(ctx):
+       await ctx.send("pong")
+
     
-def ping2(ctx):
-    ctx.send("pong2")
+intents = discord.Intents.default()
+intents.message_content = True
 
-if __name__ == "__main__":
-    bot.run(TOKEN)
+bot = DSbot(intents=intents)
+bot.run(TOKEN)
