d7c24cbbffebb0cc831e04bb127148fec79875d7
canarddu38 <71982379+canarddu38@users.noreply.github.com>
2022-08-25 16:00:18 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import discord
 import os
 
+help = """DuckSploit BOT - help
+prefix: ```ds!```"""
+
+
+
 class MyClient(discord.Client):
     async def on_ready(self):
         await bot.change_presence(activity=discord.Streaming(name="DuckSploit V1.0.8", url="https://ducksploit.com"))
     async def on_message(self, message):
-        if message.content.startswith('$hello'):
-            await message.channel.send('Hello!')
+        if message.content.startswith('ds!help'):
+            await message.channel.send(help)
 
 intents = discord.Intents.default()
 intents.message_content = True
