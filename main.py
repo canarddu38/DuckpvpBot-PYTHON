a0837fa7e8e4679c839b45d7ca392ed2d64f0267
canarddu38 <71982379+canarddu38@users.noreply.github.com>
2022-08-25 16:42:30 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,12 +7,31 @@ class MyClient(discord.Client):
         print("Ready!")
     async def on_message(self, message):
         if message.content.startswith('ds!help'):
-            embed=discord.Embed(title="DuckSploitBOT - help", description="""```**prefix: ** ds!```""", color=0x00ff44)
+            embed=discord.Embed(title="DuckSploitBOT - help", description="""```**prefix: ** ds!
+            **help: ** get commands list
+            **versions: ** get lastest versions of DuckSploit
+            **report: ** report a bug to our developpers```""", color=0x00ff44)
             await message.channel.send(embed=embed)
         elif message.content.startswith('ds!report'):
             report = message.content.replace("ds!report ", "")
             print("New report: "+report)
-            await message.channel.send("hey")
+            webhook = Webhook.from_url('webhook-url-here', adapter=RequestsWebhookAdapter())
+            embed = discord.Embed(title="New bug found", description="```"+report+"```")
+            embed.add_field(name="Author", value=message.author)
+            embed.add_field(name="Date", value=message.author)
+            webhook.send(embed=embed)
+            await message.channel.send("Your report is sended to our discord server. We'll fix it soon ;)")
+        elif message.content.startswith('ds!versions')
+            response = requests.get(f"""https://raw.githubusercontent.com/canarddu38/DUCKSPLOIT/root/hacker/windows/version.txt""")
+            version_windows = "Windows: "+response.json()
+            response = requests.get(f"""https://raw.githubusercontent.com/canarddu38/DUCKSPLOIT/root/hacker/linux/version.txt""")
+            version_linux = "Linux: "+response.json()
+            response = requests.get(f"""https://raw.githubusercontent.com/canarddu38/DUCKSPLOIT/root/hacker/android/version.txt""")
+            version_android = "Android: "+response.json()
+            
+            embed = discord.Embed(title="DuckSploit lastest versions", description="```"+version_windows+"\n"+version_linux+"\n"+version_android+"```")
+            await message.channel.send(embed=embed)
+            
 
 intents = discord.Intents.default()
 intents.message_content = True
