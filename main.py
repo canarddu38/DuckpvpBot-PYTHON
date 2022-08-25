8a2a724e93b0372a4c48c54403aed72946500d7e
canarddu38 <71982379+canarddu38@users.noreply.github.com>
2022-08-25 16:22:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ import os
 
 class MyClient(discord.Client):
     async def on_ready(self):
-        self.change_presence(activity=discord.Streaming(name="DuckSploit V1.0.8", url="https://ducksploit.com"))
+        await self.change_presence(activity=discord.Streaming(name="DuckSploit V1.0.8", url="https://ducksploit.com"))
         print("Ready!")
     async def on_message(self, message):
         if message.content.startswith('ds!help'):
-            embed=discord.Embed(title="DuckSploitBOT - help", url="https://ducksploit.com/", description="This is an embed that will show how to build an embed and the different components", color=0x00ff44)
+            embed=discord.Embed(title="DuckSploitBOT - help", description="""**help: ** ds!help""", color=0x00ff44)
             await message.channel.send(embed=embed)
         elif message.content.startswith('ds!test'):
             await message.channel.send("hey")
