93ac27d7322bae1fabd93696e1ccd39fae76f8fe
canarddu38 <71982379+canarddu38@users.noreply.github.com>
2022-08-25 15:24:15 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,15 @@
-import os
 import discord
+import os
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print(f'Logged on as {self.user}!')
+
+    async def on_message(self, message):
+        print(f'Message from {message.author}: {message.content}')
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-client = discord.Client(intents=intents)
-TOKEN = os.getenv("DISCORD_TOKEN")
-
-@client.event
-async def on_ready():
-    print(f'We have logged in as {client.user}')
-
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
-client.run(TOKEN)
+client = MyClient(intents=intents)
+client.run(os.getenv('DISCORD_TOKEN'))
