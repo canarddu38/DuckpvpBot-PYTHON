ceb10f2ec5478a19f2e6dd7c70455e2588e8bb1d
canarddu38 <71982379+canarddu38@users.noreply.github.com>
2022-08-25 20:15:55 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,25 +3,8 @@ from discord import Webhook
 import os
 import requests
 import aiohttp
-import numpy as np
-from PIL import Image, ImageChops, ImageDraw, ImageFont
-from io import BytesIO
-import asyncio
 
 class DSbot(discord.Client):
-    def circle(pfp,size = (215,215)): 
-        pfp = pfp.resize(size, Image.ANTIALIAS).convert("RGBA")
-    
-        bigsize = (pfp.size[0] * 3, pfp.size[1] * 3)
-        mask = Image.new('L', bigsize, 0)
-        draw = ImageDraw.Draw(mask) 
-        draw.ellipse((0, 0) + bigsize, fill=255)
-        mask = mask.resize(pfp.size, Image.ANTIALIAS)
-        mask = ImageChops.darker(mask, pfp.split()[-1])
-        pfp.putalpha(mask)
-        return pfp
-
-
     async def on_ready(self):
         await self.change_presence(activity=discord.Streaming(name="DuckSploit V1.0.8", url="https://ducksploit.com"))
         print("Ready!")
@@ -30,31 +13,12 @@ class DSbot(discord.Client):
     async def on_member_join(member):
         guild = bot.get_guild(880882906350882817) # <- Your Server (Guild) ID (Right-Click on Server Icon -> Copy ID)
         text = guild.get_channel(956682695100993616) # <- Your Welcome-Channel ID (Right-Click on Text-Channel -> Copy ID)
-        filename = "welcome.png" # <- The name of the file that will be saved and deleted after (Should be PNG)
+        
+        
+        
+        
+        msg = await text.send(content ="member.avatar_url_as(size=1024)\nWELCOME " + member.mention + " to DUCKSPLOIT - SUPPORT! :heart:") 
 
-        background = Image.open("welcome.png") # <- Background Image (Should be PNG)
-
-        asset = member.avatar_url_as(size=1024) # This loads the Member Avatar
-        data = BytesIO(await asset.read())
-
-        pfp = Image.open(data).convert("RGBA")
-        pfp = circle(pfp)
-        pfp = pfp.resize((265,265)) # Resizes the Profilepicture so it fits perfectly in the circle
-
-        draw = ImageDraw.Draw(background)
-        font = ImageFont.truetype("LemonMilkMedium-mLZYV.otf",42) # <- Text Font of the Member Count. Change the text size for your preference
-        member_text = ("#" + str(guild.member_count) + "  USER") # <- Text under the Profilepicture with the Membercount
-        draw.text((383,410),member_text,font=font)
-
-        background.paste(pfp, (379,123), pfp) # Pastes the Profilepicture on the Background Image
-        background.save(filename) # Saves the finished Image in the folder with the filename
-
-        msg = await text.send(file = discord.File(filename),content ="WELCOME " + member.mention + "! Please read the rules! :heart:") # <- The welcome Message Content put above the Image. "member.mention" @mentions the user
-        await asyncio.sleep(5) # 5 Seconds of waiting time
-        try: 
-            os.remove('./' + filename) # <- Change your path to where the Bot is located. Tries to delete the file again so your folder won't be full of Images. If it's already deleted nothing will happen
-        except:
-            pass
 
 
     async def on_message(self, message):
