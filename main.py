31ffbe7cc0316c68dfe7d7baaaff1d57474b0623
canarddu38 <71982379+canarddu38@users.noreply.github.com>
2022-08-25 16:01:24 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ prefix: ```ds!```"""
 
 class MyClient(discord.Client):
     async def on_ready(self):
-        await bot.change_presence(activity=discord.Streaming(name="DuckSploit V1.0.8", url="https://ducksploit.com"))
+        print("Ready!")
     async def on_message(self, message):
         if message.content.startswith('ds!help'):
             await message.channel.send(help)
